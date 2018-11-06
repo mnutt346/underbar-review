d029d6d998e4ee10418399ef94ce5848d2ed7ccc
mnutt346 <mnutt346@gmail.com>
2018-11-06 15:24:38 -0600

    Passed all tests w/in part1.js

--- spec/part1.js
+++ spec/part1.js
@@ -469,10 +469,9 @@
         var orderTraversed = [];
 
         _.reduce([1, 2, 3, 4], function(memo, item) {
-          // FILL_ME_IN
-          // Add a line here that makes this test pass
-          // for a working implementation of reduce
-          return memo;
+          console.log('1: ' + orderTraversed)
+          orderTraversed.push(item);
+          console.log('2: ' + orderTraversed)
         }, 10);
 
         expect(orderTraversed).to.eql([1, 2, 3, 4]);
