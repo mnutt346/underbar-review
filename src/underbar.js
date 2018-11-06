ca2cae8e054c37ddf67b0dfa0dd18c9ebe93b91c
mnutt346 <mnutt346@gmail.com>
2018-11-06 11:46:49 -0600

    stopped on uniq

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val
   };
 
   /**
@@ -37,6 +38,15 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if(n === undefined) {
+      return array[array.length - 1]
+    } else if(n === 0) {
+      return []
+    } else if( n > array.length) {
+      return array
+    } else {
+      return array.splice(-n)
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +55,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for(var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,16 +85,47 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var array = []
+    for(var i = 0; i < collection.length; i++) {
+      if(test(collection[i])) {
+        array.push(collection[i])
+      }
+    }    
+    
+    return array
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var array = [];
+    for (var i = 0; i < collection.length; i++) {
+      if (test(collection[i])) {
+        continue;
+      } else {
+        array.push(collection[i]);
+      }
+    }
+    return array;
+    
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    var newArray = [];
+    if(isSorted) {
+
+    } else {
+    for (var i = 0; i < array.length; i++) {
+      if(newArray.includes(array[i])) {
+        continue;
+      } else {
+        newArray.push(array[i])
+      }
+    }
+  }
+    return newArray
   };
 
 
